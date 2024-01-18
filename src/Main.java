6de126e850f83a04beb949f0772200c89a95f550
jfzara <zarajeanfabrice@gmail.com>
2024-01-17 20:14:46 -0500

    Methode pour la date

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,22 @@ System.out.println();
 private static String (){
 
     }
-       private static String obtenirValeurString(Scanner scanner, String message){
 
+    // Méthode pour obtenir une valeur de type String depuis l'utilisateur
+       private static String obtenirValeurString(Scanner scanner, String message){
+            System.out.print(message);
+            return scanner.nextLine().trim();
         }
         private static LocalDate obtenirDate(Scanner scanner, String message){
+            System.out.print(message);
+            String dateString = scanner.nextLine().trim();
 
+            try {
+                return LocalDate.parse(dateString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));//on parse la date car la saisie est un string
+            } catch (Exception e) {
+                System.out.println("Format de date incorrect. Veuillez utiliser le format AAAA-MM-JJ.");
+                return obtenirDate(scanner, message);
+            }
         }
 
         private static double calculerDevis(int prixDeBase, LocalDate dateNaissance, String){
