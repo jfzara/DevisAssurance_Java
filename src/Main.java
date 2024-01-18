6f3d6dee86c76147bcf939dd9d026fd0a6e0438e
jfzara <zarajeanfabrice@gmail.com>
2024-01-17 20:03:46 -0500

    Declaration des méthodes

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,27 @@ import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
+        String prenom, nom, marqueVoiture, codePostal ;
+        int nbreAccidents, anneeVoiture;
+        double devisBase;
 
 Scanner myScanner = new Scanner(System.in);
-String prenom, nom, marqueVoiture, codePostal ;
-int nbreAccidents, anneeVoiture;
+System.out.println();
+private static String (){
 
     }
+       private static String obtenirValeurString(Scanner scanner, String message){
+
+        }
+        private static LocalDate obtenirDate(Scanner scanner, String message){
+
+        }
+
+        private static double calculerDevis(int prixDeBase, LocalDate dateNaissance, String){
 }
+    }
+        private static String afficherDevis(String prenom, String nom, String marqueVoiture, int
+        anneeVoiture, int devis){
+
+        }
+    }
